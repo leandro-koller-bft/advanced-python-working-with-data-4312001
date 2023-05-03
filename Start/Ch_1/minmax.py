40da92cf8c2f6d179f037773e061d95ea5f3fd7b
leandro-koller-bft <leandro@brasilfone.com.br>
2023-05-03 17:58:00 -0300

    lesson 1 clear

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -3,20 +3,39 @@
 import json
 
 
-# Declare an array with some sample data in it
-values = [3.0, 2.5, 5.1, 4.1, 1.8, 1.6, 2.2, 5.7, 6.1]
-strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
+# # Declare an array with some sample data in it
+# values = [3.0, 2.5, 5.1, 4.1, 1.8, 1.6, 2.2, 5.7, 6.1]
+# strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 
 
-# TODO: The min() function finds the minimum value
+# # TODO: The min() function finds the minimum value
+# print(f"Minimum value: {min(values)}")
+# print(f"Minimum strings value: {min(strings)}")
 
+# # TODO: The max() function finds the maximum value
+# print(f"Maximum value: {max(values)}")
+# print(f"Maximum strings value: {max(strings)}")
 
-# TODO: The max() function finds the maximum value
-
-
-# TODO: define a custom "key" function to extract a data field
-
+# # TODO: define a custom "key" function to extract a data field
+# print(f"Minimum strings value: {min(strings, key=len)}")
+# print(f"Maximum strings value: {max(strings, key=len)}")
 
 # TODO: open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("./30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+
+print(data["metadata"]["title"])
+print(len(data["features"]))
+
+
+def get_mag(data_item):
+    magnitude = data_item["properties"]["mag"]
+
+    if magnitude is None:
+        magnitude = 0
+
+    return float(magnitude)
+
+
+print(min(data["features"], key=get_mag))
+print(max(data["features"], key=get_mag))
